9361778d911c0270c754d8869efd6d75fdaeffdb
KaeroDot <KaeroDot@contact.me.on.git.hub>
2016-08-16 22:22:53 +0200

    fix, add: errors in managing results in find_frequency2, fundamental is now min(pointspersection), results also all signal frequencies

--- find_frequency2.m
+++ find_frequency2.m
@@ -20,10 +20,12 @@ searchrange = 1e9;
 pointspersection = 640.*iterator*[2 4 8 16];
 
 % code length of memory section containing the fundamental frequency:
-fundcodel = pointspersection(1);
+% (let be fundamental frequency the highest one, i.e. shortest memory section.
+% it is supposed that other frequencies are integer multiple of the fundamental one)
+fundcodel = min(pointspersection(1));
 
 % periods per memory section:
-% (how many periods is contain in every memory section)
+% (how many periods is contained in every memory section)
 periodspersection = 40;
 % amplitudes per memory section:
 % (how many different amplitudes is contained in every memory section)
@@ -67,9 +69,9 @@ maxfclock = targfclock + searchrange;
 
 % generate all possible clock frequencies:
 fclock = [minfclock:clockprec:maxfclock];
-fclock = 13.76256e9
+%fclock = 13.76256e9
 % calculate fundamental signal frequency:
-fsig = fclock./fundcodel.*periodspersection./bitmult;
+ffund = fclock./fundcodel.*periodspersection./bitmult;
 % calculate compensation signal frequency:
 % (compensation signal is generated for whole memory. bit multiplication decreases frequency)
 fcomp = fclock./sum(pointspersection)./bitmult;
@@ -77,29 +79,33 @@ fcomp = fclock./sum(pointspersection)./bitmult;
 id = fix(fcomp./compprec) == fcomp./compprec;
 % select only correct results:
 fclock = fclock(id);
-fsig = fsig(id);
+ffund = ffund(id);
 fcomp = fcomp(id);
 
 % initialize result variables:
 res.fclock = [];
-res.fsig = []; 
+res.ffund = []; 
 res.fcomp = []; 
 res.adcfs = {};
+res.fsig = {}; 
+% through all found clock frequencies:
 for i = 1:length(fclock)
     % check if current fundamental signal frequency can be coherently sampled by ADC:
-    % (coherency: curfsig/adcfs = periods/samples = rational number,
+    % (coherency: curffund/adcfs = periods/samples = rational number,
     % so both periods and samples have to be an integer)
     % periods is known, thus samples must be integer:
-    samples = periodsperamp.*adcfs./fsig(i);
+    samples = periodsperamp.*adcfs./ffund(i);
     cohind = fix(samples) == samples;
-    %if any(cohind)
+    if any(cohind)
         res.fclock = [res.fclock fclock(i)];
-        res.fsig = [res.fsig fsig];
-        res.fcomp = [res.fcomp fcomp];
+        res.ffund = [res.ffund ffund(i)];
+        res.fcomp = [res.fcomp fcomp(i)];
         tmp = find(cohind);
         res.adcfs = [res.adcfs {adcfs(tmp)}];
+        % calculate all signal frequencies:
+        res.fsig = [res.fsig {fclock(i)./pointspersection.*periodspersection./bitmult}];
         disp(['found results: ' num2str(length(res.fclock))])
-    %end
+    end
 end
 
 % find result nearest to the target clock frequency:
@@ -107,20 +113,28 @@ id = find(min(abs(res.fclock - targfclock)) == abs(res.fclock - targfclock));
 if isempty(id)
     disp(['no result found for iterator value ' num2str(iterator) ' ! you can increase search range...'])
     res.fclock = [];
+    res.ffund = [];
     res.fsig = [];
     res.fcomp = [];
     res.adcfs = [{}];
 else
     id = id(1);
+    res.fclock = res.fclock(id);
+    res.ffund = res.ffund(id);
+    res.fcomp = res.fcomp(id);
+    res.adcfs = res.adcfs(id);
+    res.fsig = res.fsig(id);
     disp(['results for iterator value ' num2str(iterator) ' :'])
     disp('clock frequency (GHz):')
-    res.fclock(id)/1e9
+    res.fclock/1e9
     disp('signal fundamental frequency (Hz):');
-    res.fsig(id)
+    res.ffund
     disp(['compensation frequency for ' num2str(bitmult) ' bit multiplication (Hz):']);
-    res.fcomp(id)
+    res.fcomp
     disp('list of ADC sampling frequencies (Hz):');
-    res.adcfs{id}
+    res.adcfs
+    disp('list of all signal frequencies (Hz):');
+    res.fsig
 endif
 
 disp('finished')
